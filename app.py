768b458432e592a4a926e907189ecdd792582b2d
Thanyarat-may <sriphomma3501@gmail.com>
2025-04-20 11:53:01 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ Created on Sun Apr 20 10:44:11 2025
 
 @author: LAB
 """
-
 #import library
 import streamlit as st
 import numpy as np
@@ -14,18 +13,15 @@ import pickle
 with open('dtm_trained_model.pkl','rb') as f:
     dtm_model = pickle.load(f)
     
-    
 #application title
-st.title("Iris flower Classification") 
+st.title("Iris flower Classification")
 st.write("Enter the feature of the iris flower:")
 
-
-#Input fields 
-sepal_length = st.slider("Sepal Length (cm)",4.0,8.0,5.1)  
+#Input fields
+sepal_length = st.slider("Sepal Length (cm)",4.0,8.0,5.1)
 sepal_width = st.slider("Sepal Width (cm)",2.0,4.5,3.5)
 petal_length = st.slider("Petal Length (cm)",1.0,7.0,1.4)
 petal_width = st.slider("Petal Width (cm)",0.1,2.5,0.2)
-
 
 #Predict button
 if st.button("Predict"):
